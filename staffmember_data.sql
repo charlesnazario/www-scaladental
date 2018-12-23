2a9c55d45df7a0dcc4afd708431a3f3e9ab94780
Charles Nazario <charlie@nazarios.net>
2018-12-22 22:21:19 -0500

    Added initial staff data used for populating database

---
+++ staffmember_data.sql
@@ -0,0 +1,28 @@
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (1, 'dantescala', 'Dante Scala, DMD', 'Dentist', 'dantescala.jpg', '<p>
+Dr. Dante Scala is a lifelong resident of Rockland County, New York, having graduated from Clarkstown South High School.  He is a graduate of Farleigh Dickinson University in Hackensack, New Jersey with a Bachelor of Science degree in Biology, magna cum laude, and  Fairleigh Dickinson University School of Dentistry in New Jersey, where he earned his Doctor of Dentistry degree and NYS license to practice in 1986.
+</p>
+<p>
+Dr. Scala established and has maintained his own general dentistry family practice since 1988 involved in all aspects of dental care.
+</p>
+<p>
+Several years later, his brother, Dr. Joseph Scala partnered with him.   In 2018 they relocated into a brand new state of the art dental office in Pearl River, New York.   Dr. Scala is a member of the Rockland County Dental Society, the Ninth District Dental Association of New York, the New York State Dental Association, and the American Dental Association.
+</p>');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (2, 'josephscala', 'Joseph Scala, DMD', 'Dentist', 'josephscala.jpg', '<p>
+Dr. Joseph Scala is a lifelong resident of Rockland County, New York, having graduated Clarkstown South High School.  He then graduated Fairleigh Dickinson University in Hackensack, New Jersey with a  Bachelor of Science degree in Biology and Fairleigh Dickinson University School of Dentistry in New Jersey, where he earned his Doctor of Dentistry degree and NYS license to practice in 1989.
+</p>
+<p>
+Dr. Joseph Scala graduated the General Practice Residency program at NYU Medical Center in 1990, where he received advanced post doctorate training.
+</p>
+<p>
+He has been partnered with his brother, Dr. Dante Scala, since 1991 in Pearl River, New York.  Together they maintain a general dentistry family practice involved in all aspects of dental care.  Dr. Joseph Scala is a member of the Rockland County Dental Society, the Ninth District Dental Association of New York, the New York State Dental Association, and the American Dental Association.
+</p>');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (3, 'jenna', 'Jenna', 'Dental Hygienist', 'jenna.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (4, 'michelle', 'Michelle', 'Office Manager', 'michelle.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (5, 'jennifer', 'Jennifer', 'Billing Coordinator', 'jennifer.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (6, 'kayla', 'Kayla', 'Dental Assistant', 'kayla.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (7, 'carleigh', 'Carleigh', 'Dental Assistant', 'carleigh.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (8, 'diana', 'Diana', 'Dental Assistant', 'diana.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (9, 'kerry', 'Kerry', 'Dental Hygienist', 'kerry.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (10, 'amanda', 'Amanda', 'Dental Hygienist', 'amanda.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (11, 'lillian', 'Lillian', 'Administrator', 'lillian.jpg', 'NA');
+INSERT INTO main_staffmember (id, name_key, name, title, photo_src, biography) VALUES (12, 'dolores', 'Dolores', 'Dental Assistant', 'dolores.jpg', 'NA');
